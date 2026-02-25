4b9b69fbfb80475b5dc020c9b7bd39159909fad7
Dawid Kozak <d.kozak92@gmail.com>
2026-02-25 17:57:56 +0100

    feat: add initial commit with base project and project documentation

---
+++ plugins/vuetify.ts
@@ -0,0 +1,12 @@
+import { createVuetify } from "vuetify";
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+export default defineNuxtPlugin((app) => {
+  const vuetify = createVuetify({
+    theme: {
+      defaultTheme: "dark",
+    },
+  });
+  app.vueApp.use(vuetify);
+});
